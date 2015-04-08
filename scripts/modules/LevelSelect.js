1b0ec1293401c6fd71347a19266e4150545ab1df
mastermalone <michael.malone@aofl-00015.aofl.local>
2015-04-07 18:10:07 -0700

    Created the directory which will contain the modules that are rendered in the puzzles game engin view

---
+++ scripts/modules/LevelSelect.js
@@ -0,0 +1,60 @@
+(function () {
+	 define(["CreateNode"], function (CreateNode) {
+	     "use strict";
+
+	 	function LevelSelect () {
+	 		//Empty Constructor
+	 	}
+	 	
+	 	LevelSelect.prototype = {
+	 		constructor: LevelSelect,
+	 		setURL: function (url) {
+                return {
+                    url: url
+                };  
+	 		},
+	 		placeContent: function (el, content) {
+	 			if(!el || el === ""){
+	 				console.log("LevelSelect Module: You did not pass in the ID of the target element");
+	 				return;
+	 			}
+	 			this.el = el;
+	 			var targetElm = document.getElementById(this.el);
+	 			targetElm.appendChild(content);
+	 			//return targetElm;
+	 		},
+	 		animateLevels: function (Bool) {
+	 			this.animate = Bool;
+	 			if(!this.animate){
+	 				console.log("You did not set this to true");
+	 				return;
+	 			}
+	 			console.log("Got passed the check for bool", this.animate);
+	 		},
+	 		render: function (data) {
+	 		    console.log("RENDER CONTENT");
+	 			console.log("Rendiering the content", data);
+	 			//Create elements for the level select view based on the data object returned
+	 			var i, levels = data, length = Object.keys(data[Object.keys(data)]).length, holder = new CreateNode(), hld, selectBtn = new CreateNode(), sb, selector = new CreateNode(), slctr, frag = document.createDocumentFragment();
+	 			
+	 			console.log("VALUE OF FIRST CHILD", Object.keys(data[Object.keys(data)]).length);
+	 			hld = holder.makeElement("DIV", "id", "level-select");
+	 			slctr = selector.makeElement("SPAN", "id", "selector", "1", true);
+	 			
+	 			for(i = 0; i < length; i++){
+	 			    //console.log("length of the object", levels["level"+(i+1)].image);
+	 			    //console.log("length of the object", levels["level"+(i+1)].completed);
+	 			    sb = selectBtn.makeElement("DIV", "id", "select-btn"+(i+1), (i+1+""));
+	 			    frag.appendChild(sb);
+	 			}
+                frag.appendChild(slctr);
+	 			hld.appendChild(frag);
+	 			
+	 			this.placeContent("main", hld);
+	 			//return hld;
+	 		}
+	 	};
+	 	
+	 	return LevelSelect;
+	 });
+})();
---
+++ scripts/puzzle-config.js
@@ -0,0 +1,27 @@
+require.config({
+    urlArgs: "bust=" +  (new Date()).getTime(),//Remove before deployment
+    baseUrl:"scripts/",
+    paths: {
+        "App": "../../game-modules/scripts/app",
+        "Ajax": "../../game-modules/scripts/modules/Ajax",
+        "BaseController": "../../game-modules/scripts/controllers/BaseController",
+        "BaseModel": "../../game-modules/scripts/models/BaseModel",
+        "BaseView": "../../game-modules/scripts/views/BaseView",
+        "CreateNode": "../../game-modules/scripts/modules/CreateNode",
+        "Events": "../../game-modules/scripts/modules/Events",
+        "jquery": "../../game-modules/scripts/lib/jquery.min",
+        "LevelSelect": "modules/LevelSelect",
+        "LevelSelectView": "../../game-modules/scripts/views/LevelSelectView",
+        "LevelSelectController": "../../game-modules/scripts/controllers/LevelSelectController",
+        "LevelSelectModel": "../../game-modules/scripts/models/LevelSelectModel",
+        "Subclass": "../../game-modules/scripts/modules/Subclass"
+    }
+});
+
+
+require(["LevelSelect","App"], function (LevelSelect, App) {
+    var lvs = new LevelSelect(), api;
+    api = lvs.setURL('../../game-modules/json/game.json');
+    App.init(api.url);
+    console.log("THE API Url", api.url);
+});
