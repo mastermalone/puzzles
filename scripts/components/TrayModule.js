96ac31f49de8e4108613520ea16b1fb171e7887e
mastermalone <mastermalone@mastermalonebitfenix.(none)>
2015-05-12 22:33:14 -0700

    Preparing the tray containier.

--- scripts/components/TrayModule.js
+++ scripts/components/TrayModule.js
@@ -1,10 +1,15 @@
 (function () {
-    define([], function () {
+    define(["DOMElement"], function (DOMElement) {
         "use strict";
         
         var TrayModule = {
             render: function (data) {
                 console.log("RENDERING TRAY");
+                
+                var tray = new DOMElement({
+                    type: ["DIV"],
+                    id: ["tray"]
+                });
             }
         };
         
